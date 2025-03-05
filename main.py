3f3389f40394ecbd38952868d402215adce27e1a
b0lbas <sumericforden@gmail.com>
2025-03-05 14:14:09 +0100

    i did something !! (??)

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+class MyFlaskApp:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.setup_routes()
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    def setup_routes(self):
+        @self.app.route('/')
+        def home():
+            return render_template('index.html')
+
+    def run(self):
+        self.app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app_instance = MyFlaskApp()
+    app_instance.run()
