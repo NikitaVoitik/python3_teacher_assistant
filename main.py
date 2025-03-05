9ce310228b6c4b155981944c35806b6fcf654843
b0lbas <sumericforden@gmail.com>
2025-03-05 15:17:47 +0100

    everything possible added

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from llm_api import LLMClient
 
 class MyFlaskApp:
     def __init__(self):
         self.app = Flask(__name__)
+        self.llm_client = LLMClient(api_key='s0bCxiZAttwKRcBbqdyHhLxpjUjRDZWO', model='mistral-large-latest')
         self.setup_routes()
 
     def setup_routes(self):
         @self.app.route('/')
         def home():
             return render_template('index.html')
+
+        @self.app.route('/ask', methods=['POST'])
+        def ask():
+            data = request.json
+            user_input = data.get('message')
+            system_prompt = data.get('systemPrompt')
+            response = self.llm_client.get_response(user_input, system_prompt)
+            return jsonify({'response': response})
 
     def run(self):
         self.app.run(debug=True)
