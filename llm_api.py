7d391b9e8dd2125387a893f740fbbb51a4c90822
nikitavoitik <nikitavoitik2006@gmail.com>
2025-03-07 14:14:20 +0100

    Changed images recognition to mistral one

--- llm_api.py
+++ llm_api.py
@@ -10,10 +10,7 @@ class LLMClient:
     def get_response(self, user_input, system_prompt):
         self.chat_history.append({"role": "user", "content": user_input})
         messages = [{"role": "system", "content": system_prompt}] + self.chat_history
-        response = self.mistral_client.chat.complete(
-            model=self.model,
-            messages=messages
-        )
+        response = self.mistral_client.chat.complete(model=self.model, messages=messages)
         response_content = response.choices[0].message.content
         self.chat_history.append({"role": "assistant", "content": response_content})
         return response_content
@@ -21,6 +18,14 @@ class LLMClient:
     def add_file(self, file_name, file_content):
         prefix = f"Filename: {file_name}\nFile Content:\n"
         self.chat_history.append({"role": "user", "content": prefix + file_content})
+
+    def add_image(self, image):
+        if self.model != "pixtral-large-latest":
+            self.model = "pixtral-large-latest"
+        if isinstance(image, bytes):
+            image = image.decode('utf-8')
+        self.chat_history.append({"role": "user",
+            "content": [{"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}]})
 
     def add_message(self, message):
         self.chat_history.append({"role": "user", "content": message})
